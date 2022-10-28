24427f0ae0bde5f6f57b6f9b392c631c89182ac8
Helen Lehrer <hlehrer@outlook.com>
2022-10-28 14:05:47 -0700

    Add Repository repo & JWT files

---
+++ PdxLocalBusinesses/Repository/JWTManagerRepository.cs
@@ -0,0 +1,37 @@
+public class JWTManagerRepository : IJWTManagerRepository
+{
+  //can do this with a database
+	Dictionary<string, string> UsersRecords = new Dictionary<string, string>
+	{
+		{ "user1","password1"},
+		{ "user2","password2"},
+		{ "user3","password3"},
+	};
+
+	private readonly IConfiguration iconfiguration;
+	public JWTManagerRepository(IConfiguration iconfiguration)
+	{
+		this.iconfiguration = iconfiguration;
+	}
+	public Tokens Authenticate(Users users)
+	{
+		if (!UsersRecords.Any(x => x.Key == users.Name && x.Value == users.Password)) {
+			return null;
+		}
+
+		// Else we generate JSON Web Token
+		var tokenHandler = new JwtSecurityTokenHandler();
+		var tokenKey = Encoding.UTF8.GetBytes(iconfiguration["JWT:Key"]);
+		var tokenDescriptor = new SecurityTokenDescriptor
+		{
+		  Subject = new ClaimsIdentity(new Claim[]
+		  {
+			 new Claim(ClaimTypes.Name, users.Name)                    
+		  }),
+		   Expires = DateTime.UtcNow.AddHours(48),
+		   SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey),SecurityAlgorithms.HmacSha256Signature)
+		};
+		var token = tokenHandler.CreateToken(tokenDescriptor);
+		return new Tokens { Token = tokenHandler.WriteToken(token) };
+
+	}
