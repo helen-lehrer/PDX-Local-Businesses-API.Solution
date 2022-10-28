647c5606efc2ba0dd98dce3138431fd06ff535e5
Helen Lehrer <hlehrer@outlook.com>
2022-10-28 09:53:58 -0700

    Add PortlandLocalBusinessesContext and db seed data

--- PortlandLocalBusinesses/Models/Business.cs
+++ PortlandLocalBusinesses/Models/Business.cs
@@ -10,8 +10,8 @@ namespace PortlandLocalBusinesses.Models
   {
     public int BusinessId { get; set; }
     public string Name { get; set; }
-    public string Hours { get; set; }
-    public string Address { get; set; }
+    public string Description { get; set; }
+    public string PhoneNumber { get; set; }
     public int NeighborhoodId { get; set; }
 		// public Neighborhood Neighborhood { get; set; }
 	
--- PortlandLocalBusinesses/Models/PortlandLocalBusinessesContext.cs
+++ PortlandLocalBusinesses/Models/PortlandLocalBusinessesContext.cs
@@ -1,85 +1,119 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Proxies;
-// using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+using System;
 
-// namespace PortlandLocalBusinesses.Models
-// {
-// 	public class PortlandLocalBusinessesContext : DbContext
-// 	{
-// 		public PortlandLocalBusinessesContext(DbContextOptions<PortlandLocalBusinessesContext> options) : base(options)
-// 		{
+namespace PortlandLocalBusinesses.Models
+{
+	public class PortlandLocalBusinessesContext : DbContext
+	{
+		public PortlandLocalBusinessesContext(DbContextOptions<PortlandLocalBusinessesContext> options) : base(options)
+		{
 
-// 		}
+		}
 
-// 		public DbSet<Message> Messages {get; set;}
-// 		public DbSet<Group> Groups {get; set;}
+		public DbSet<Business> Businesses {get; set;}
+		public DbSet<Neighborhood> Neighborhoods {get; set;}
 
-//     protected override void OnModelCreating(ModelBuilder builder)
-//     {
-//       builder.Entity<Group>()
-//         .HasData(
-//           new Group { GroupId = 1, Name= "Halloweenie Parties" },
-//           new Group { GroupId = 2, Name= "Video Gaymezzzzz" },
-//           new Group { GroupId = 3, Name= "Locate the PSL goodies" }
-//         );
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Neighborhood>()
+        .HasData(
+          new Neighborhood { NeighborhoodId = 1, Name= "Northwest Portland" },
+          new Neighborhood { NeighborhoodId = 2, Name= "Pearl District" },
+          new Neighborhood { NeighborhoodId = 3, Name= "Goose Hollow Foothills League" },
+          new Neighborhood { NeighborhoodId = 4, Name= "Portland Downtown" },
+          new Neighborhood { NeighborhoodId = 5, Name= "Laurelhurst" },
+          new Neighborhood { NeighborhoodId = 6, Name= "Arlington Heights" },
+          new Neighborhood { NeighborhoodId = 7, Name= "Sellwood - Moreland" },
+          new Neighborhood { NeighborhoodId = 8, Name= "Eastmoreland" },
+          new Neighborhood { NeighborhoodId = 9, Name= "Irvington" },
+          new Neighborhood { NeighborhoodId = 10, Name= "Southwest Hills" },
+          new Neighborhood { NeighborhoodId = 11, Name= "Hosford-Abernethy" },
+          new Neighborhood { NeighborhoodId = 12, Name= "Northwest Heights" },
+          new Neighborhood { NeighborhoodId = 13, Name= "Beaumont - Wilshire" },
+          new Neighborhood { NeighborhoodId = 14, Name= "Healy Heights" },
+          new Neighborhood { NeighborhoodId = 15, Name= "Kerns" },
+          new Neighborhood { NeighborhoodId = 16, Name= "South Portland" },
+          new Neighborhood { NeighborhoodId = 17, Name= "Forest Park" },
+          new Neighborhood { NeighborhoodId = 18, Name= "Sylvan - Highlands" },
+          new Neighborhood { NeighborhoodId = 19, Name= "Old Town" },
+          new Neighborhood { NeighborhoodId = 20, Name= "Sullivan's Gulch" },
+          new Neighborhood { NeighborhoodId = 21, Name= "Marshall Park" },
+          new Neighborhood { NeighborhoodId = 22, Name= "Northwest Industrial" },
+          new Neighborhood { NeighborhoodId = 23, Name= "Grant Park" },
+          new Neighborhood { NeighborhoodId = 24, Name= "Northeast Portland" },
+          new Neighborhood { NeighborhoodId = 25, Name= "Hollywood" },
+          new Neighborhood { NeighborhoodId = 26, Name= "Buckman" },
+          new Neighborhood { NeighborhoodId = 27, Name= "Arnold Creek" },
+          new Neighborhood { NeighborhoodId = 28, Name= "Collins View" },
+          new Neighborhood { NeighborhoodId = 29, Name= "South Waterfront" },
+          new Neighborhood { NeighborhoodId = 30, Name= "Mount Tabor" },
+          new Neighborhood { NeighborhoodId = 31, Name= "Multnomah" },
+          new Neighborhood { NeighborhoodId = 32, Name= "Overlook" },
+          new Neighborhood { NeighborhoodId = 33, Name= "Hawthorne" },
+          new Neighborhood { NeighborhoodId = 34, Name= "St.Johns" },
+          new Neighborhood { NeighborhoodId = 35, Name= "Boise" },
+          new Neighborhood { NeighborhoodId = 36, Name= "Central Eastside" },
+          new Neighborhood { NeighborhoodId = 37, Name= "Nob Hill" },
+          new Neighborhood { NeighborhoodId = 38, Name= "Montavilla" },
+          new Neighborhood { NeighborhoodId = 39, Name= "Southeast Portland" },
+          new Neighborhood { NeighborhoodId = 40, Name= "Pleasant Valley" },
+          new Neighborhood { NeighborhoodId = 41, Name= "Lloyd District" },
+          new Neighborhood { NeighborhoodId = 42, Name= "Creston - Kenilworth" },
+          new Neighborhood { NeighborhoodId = 43, Name= "University Park" },
+          new Neighborhood { NeighborhoodId = 44, Name= "Cully" },
+          new Neighborhood { NeighborhoodId = 45, Name= "Southwest Portland" },
+          new Neighborhood { NeighborhoodId = 46, Name= "Sabin" },
+          new Neighborhood { NeighborhoodId = 47, Name= "Alameda" },
+          new Neighborhood { NeighborhoodId = 48, Name= "Richmond" },
+          new Neighborhood { NeighborhoodId = 49, Name= "Sunnyside" },
+          new Neighborhood { NeighborhoodId = 50, Name= "Hillsdale" },
+        );
 
-// 			builder.Entity<Message>()
-// 				.HasData(
-// 					new Message
-// 					{ 
-// 						MessageId = 1, 
-// 						Title = "Scary Party???", 
-// 						Body = "I'm looking for the spookiest party of all, please help???", 
-// 						Author = "GhostiniLinguini",
-// 						Date = new DateTime (2006, 6, 6),
-// 						GroupId = 1
-// 					},
-// 					new Message
-// 					{ 
-// 						MessageId = 2, 
-// 						Title = "No ghosts please", 
-// 						Body = "I hate ghosts!!!  I want to go to a halloween party but if I see a ghost I'll p** my pants, please don't recommend parties w/ ghosts", 
-// 						Author = "NoScariesPlease",
-// 						Date = new DateTime (2010, 10, 29),
-// 						GroupId = 1
-// 					},
-// 					new Message
-// 					{ 
-// 						MessageId = 3, 
-// 						Title = "Gaming!!!!", 
-// 						Body = "Holy CRUD I love gaming, particularly as a non-heterosexual/non-binary person.  It RULES!!!  I love Mountain Dew, also (unrelated).", 
-// 						Author = "DewForMeAndU",
-// 						Date = new DateTime (2003, 12, 18),
-// 						GroupId = 2
-// 					},
-// 					new Message
-// 					{ 
-// 						MessageId = 4, 
-// 						Title = "GAMING!!!!", 
-// 						Body = "It's been ten years and I'm still gaming.  Darn, it still rules so hard.  Still not straight btw.", 
-// 						Author = "DewForMeAndU",
-// 						Date = new DateTime (2013, 12, 18),
-// 						GroupId = 2
-// 					},
-// 					new Message
-// 					{ 
-// 						MessageId = 5, 
-// 						Title = "Magical Drinks Needed!", 
-// 						Body = "I'm looking for a magician that serves alchemical potions for all my spooky spells.", 
-// 						Author = "WitchyTwitchy123",
-// 						Date = new DateTime (1700, 10, 31),
-// 						GroupId = 1
-// 					},
-// 					new Message
-// 					{ 
-// 						MessageId = 6, 
-// 						Title = "Trader Joes PSL?", 
-// 						Body = "I'm looking for the best Trader Joe's PSL treats. Please Help!!", 
-// 						Author = "PSLlov3rkissessxoxo",
-// 						Date = new DateTime (2020, 11, 2),
-// 						GroupId = 3
-// 					}
-// 				);
-//     }
-//   }
-// }
+			builder.Entity<Business>()
+				.HasData(
+					new Business
+					{ 
+						BusinessId = 1, 
+						Name = "Abigayle Tarsches Photography",
+            Description = "Portrait, Branding, and Boudoir Photography",  
+						PhoneNumber = "415-609-7914", 
+						NeighborhoodId = 1
+					},
+          new Business
+					{ 
+						BusinessId = 2, 
+						Name = "Gladys Bikes",
+            Description = "Full service bike shop",  
+						PhoneNumber = "971-373-8388", 
+						NeighborhoodId = 1
+					},
+          new Business
+					{ 
+						BusinessId = 3, 
+						Name = "Moods of Florence Italy",
+            Description = "Women's clothing, costume jewelry, accessories, fashion face mask",  
+						PhoneNumber = "503-740-1874", 
+						NeighborhoodId = 19
+					},
+          new Business
+					{ 
+						BusinessId = 4, 
+						Name = "Shoe",
+            Description = "Local family owned shoe repair in the Hollywood District",  
+						PhoneNumber = "503-206-6455", 
+						NeighborhoodId = 25
+					},
+					new Business
+					{ 
+						BusinessId = 5, 
+						Name = "Photo to Canvas",
+            Description = "Your photos printed on Canvas and Fine Art Papers",  
+						PhoneNumber = "503-896-5643", 
+						NeighborhoodId = 41
+					},
+
+				);
+    }
+  }
+}
