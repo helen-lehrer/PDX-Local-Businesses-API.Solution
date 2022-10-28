162fff2616eec37c33c238983d5fa8be301bffa9
Helen Lehrer <hlehrer@outlook.com>
2022-10-28 09:16:32 -0700

    Add Business Model

---
+++ PortlandLocalBusinesses/Models/Business.cs
@@ -0,0 +1,29 @@
+using System; 
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PortlandLocalBusinesses.Models
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+    public string Name { get; set; }
+    public string Hours { get; set; }
+    public string Address { get; set; }
+    public int NeighborhoodId { get; set; }
+		// public Neighborhood Neighborhood { get; set; }
+	
+    public static List<Business> GetBusinesses()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Business> businessList = JsonConvert.DeserializeObject<List<Business>>(jsonResponse.ToString());
+
+      return businessList;
+    }
+  }
+}
